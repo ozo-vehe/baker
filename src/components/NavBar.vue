0e19cdd25d23832787c920aaad6671bb42c3d765
ozo-vehe <johnnie.vehe@gmail.com>
2023-01-17 01:05:13 +0100

    push code to github

---
+++ src/components/NavBar.vue
@@ -0,0 +1,37 @@
+<script setup>
+  import { RouterLink } from 'vue-router'
+  import { reactive, ref } from 'vue';
+
+  // Data
+  const links = reactive([
+    {link: '/home', name: 'home'},
+    {link: '/about', name: 'about'},
+    {link: '/services', name: 'services'},
+    {link: '/products', name: 'products'},
+    {link: '/contact', name: 'contact'},
+  ]);
+  let active = ref(false);
+</script>
+
+<template>
+  <nav class="bg-gray-900 text-gray-200 flex justify-between items-center px-12 py-8">
+    <RouterLink to="/home">
+      <h1 class="text-orange-200 text-blue-600 text-4xl">Baker</h1>
+    </RouterLink>
+    <div>
+      <ul class="flex gap-10">
+        <li
+          class="px-2 py-1 capitalize rounded active"
+          v-for="link in links">
+          <RouterLink :to="link.link"> {{ link.name }} </RouterLink>
+        </li>
+      </ul>
+    </div>
+  </nav>
+</template>
+
+<style scoped>
+  .active:hover {
+    color: orange;
+  }
+</style>

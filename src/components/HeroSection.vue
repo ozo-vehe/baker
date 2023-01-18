79971fb5a9d4d21bf055c09ea892277d658cea57
ozo-vehe <johnnie.vehe@gmail.com>
2023-01-18 23:47:54 +0100

    continued the about section of the landing page

--- src/components/HeroSection.vue
+++ src/components/HeroSection.vue
@@ -1,9 +1,11 @@
 <script setup>
+  import VideoSection from './VideoSection.vue';
 </script>
 
 <template>
   <main class="min-h-screen text-white border border-red-900 overflow-hidden relative flex flex-wrap flex-col justify-center px-12">
-    <video class="absolute top-0 left-0 w-full" src="../assets/videos/hero_section.mp4" autoplay loop></video>
+    <VideoSection class="absolute top-0 left-0 w-full" videoSrc="../assets/videos/hero_section.mp4" />
+    <!-- <img class="absolute top-0 left-0 w-full" src="../assets/images/carousel-1.jpg" alt="Carousel"> -->
     <!-- <h1 class="z-10 text-9xl mb-4 font-bold text-orange-400">Baker</h1> -->
     <h3 class="z-10 text-9xl mb-2 capitalize font-bold text-orange-400">We bake with passion</h3>
     <p class="z-10 font-bold text-2xl">We offer from our oven to your table, freshly baked goods for every taste bud.</p>
@@ -11,13 +13,13 @@
 </template>
 
 <style scoped>
-  main::after {
+  /* main::after {
     content: "";
     position: absolute;
     top: 0px;
     left: 0px;
     width: 100%;
     height: 100%;
-    background-color: #0000008c;
-  }
+    background-color: #0000004d;
+  } */
 </style>
---
+++ src/components/VideoSection.vue
@@ -0,0 +1,16 @@
+<script setup>
+  defineProps({
+    videoSrc: String,
+    class: String
+  })
+</script>
+
+<template>
+  <video
+    :class="class"
+    :src="videoSrc"
+    autoplay
+    loop
+  >
+  </video>
+</template>

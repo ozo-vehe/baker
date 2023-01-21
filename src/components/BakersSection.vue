e2cdc7e9901059c308aad9d7c3bcc2d9e0b86c86
ozo-vehe <johnnie.vehe@gmail.com>
2023-01-21 19:37:09 +0100

    added images to the bakers section of landing page

--- src/components/BakersSection.vue
+++ src/components/BakersSection.vue
@@ -1,9 +1,34 @@
 <script setup>
+  import { reactive } from 'vue';
+  import chef1 from '../assets/images/team-3.jpg';
+  import chef2 from '../assets/images/team-1.jpg';
+  import chef3 from '../assets/images/team-2.jpg';
+  import chef4 from '../assets/images/team-4.jpg';
+
+  const chefs = reactive([
+    {firstname: "Chef", lastname: "1", position: "head baker", image: chef1},
+    {firstname: "Chef", lastname: "2", position: "pastry chef", image: chef2 },
+    {firstname: "Chef", lastname: "3", position: "pastry chef", image: chef3 },
+    {firstname: "Chef", lastname: "4", position: "pastry chef", image: chef4 },
+])
 </script>
 
 <template>
-  <main>
-    <h3>Our Members</h3>
-    <h1></h1>
+  <main class="px-24 pb-16">
+    <h1 class="font-cassandra text-orange-400 text-center text-2xl mt-8 mb-2">Our Members</h1>
+    <h3 class="pt-2 text-center text-4xl font-bold capitalize"> Meet the team behind Baker </h3>
+     <p class="text-lg border w-550 text-center mx-auto">
+      We are a group of passionate bakers, pastry chefs, and food enthusiasts 
+      who are dedicated to creating delicious and beautiful baked goods for our customers.
+    </p>
+    <div class="pt-8 border w-full">
+      <div v-for="chef in chefs">
+        <div>
+          <img :src="chef.image" :alt="chef.firstname"> 
+        </div>
+        <h2>{{ chef.firstname }}{{ chef.lastname }}</h2>
+        <p>{{ chef.position }}</p>
+       </div>
+    </div>
   </main>
 </template>

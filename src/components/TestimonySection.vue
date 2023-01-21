0748ed83da147e2556aca1c062965074551b9936
ozo-vehe <johnnie.vehe@gmail.com>
2023-01-21 22:41:40 +0100

    added testimony images and started the slideshow

--- src/components/TestimonySection.vue
+++ src/components/TestimonySection.vue
@@ -1,8 +1,36 @@
 <script setup>
+  import { reactive, ref, computed } from 'vue';
+  import client1 from '../assets/images/testimonial-1.jpg';
+  import client2 from '../assets/images/testimonial-2.jpg';
+  import client3 from '../assets/images/testimonial-3.jpg';
+  import client4 from '../assets/images/testimonial-4.jpg';
+  import client5 from '../assets/images/testimonial-5.jpg';
+  
+  const clients = reactive([
+    {name: "Client 1", image: client1, testimony: "The bakery's warm and inviting atmosphere is always a pleasure to be in, and the delicious smell of fresh baked goods is always in the air. The breads are always fresh and baked to perfection, and the cakes and pastries are absolutely divine."},
+    {name: "Client 2", image: client2, testimony: "The staff is always friendly and helpful, and they always make sure to go above and beyond to make my experience at the bakery as enjoyable as possible. I highly recommend Baker to anyone looking for delicious baked goods and great customer service."},
+    {name: "Client 3", image: client3, testimony: "I have been a loyal customer of Baker bakery for years and I cannot recommend them enough. Their pastries are always fresh and delicious, and their cakes are truly works of art."},
+    {name: "Client 4", image: client4, testimony: "The flavors are always spot on and the presentation is always stunning. If you are looking for a bakery that truly cares about their customers and their craft, look no further than Baker bakery."},
+    {name: "Client 5", image: client5, testimony: "I am consistently impressed by the quality of their baked goods.  I highly recommend this bakery to anyone looking for delicious and high-quality baked goods."},
+  ]);
+
+  let currentIndex = ref(0)
+  const currentClient = computed(() => {
+    return clients[Math.abs(currentIndex.value) % clients.length];
+  });
 </script>
 
 <template>
-  <main>
-    Testimony
+  <main class="px-24 pb-16">
+    <h1 class="font-cassandra text-orange-400 text-center text-2xl mt-8 mb-2">Client's Reviews</h1>
+    <div>
+      <transition-group name="fade" tag="div">
+        <div v-for="i in [currentIndex]" :key="i">
+          <img :src="currentClient.image" />
+        </div>
+      </transition-group>
+      <a class="prev" @click="prev" href="#">&#10094; Previous</a>
+      <a class="next" @click="next" href="#">&#10095; Next</a>
+    </div>
   </main>
 </template>
